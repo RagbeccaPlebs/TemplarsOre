6577f9b08b3ae2efc636d219958333d16ef20500
Rebecca <60572595+RagbeccaPlebs@users.noreply.github.com>
2021-02-23 20:52:06 +0100

    Ignore

--- src/main/java/com/templarsofmortals/templarsore/core/registries/Ore.java
+++ src/main/java/com/templarsofmortals/templarsore/core/registries/Ore.java
@@ -1,7 +1,11 @@
 package com.templarsofmortals.templarsore.core.registries;
 
+import com.templarsofmortals.templarscore.common.blocks.StoneBlock;
 import com.templarsofmortals.templarsore.TemplarsOre;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
@@ -15,14 +19,16 @@ public class Ore {
     static {
         blocks = new HashMap<>();
         items = new HashMap<>();
-        registerItem("test");
+        registerStuff("test");
     }
 
     public static final HashMap<String, RegistryObject<Block>> blocks;
     public static final HashMap<String, RegistryObject<Item>> items;
 
-    public static void registerItem(String name) {
-        items.put(name, TemplarsOre.REGISTRY_HELPER.createItem(name, () -> new Item(new Item.Properties().group(ItemGroup.BREWING))));
+    public static void registerStuff(String name) {
+        blocks.put(name, TemplarsOre.REGISTRY_HELPER.createBlock(name, () ->
+                new StoneBlock(AbstractBlock.Properties.create(Material.ROCK)
+                        .sound(SoundType.SOUL_SAND)), ItemGroup.BREWING));
     }
 
 }
